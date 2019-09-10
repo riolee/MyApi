e33816c81f08dd6bbcd9e2fe1ca96fe3f3ae0c77
Camaro <cstcamaro@gmail.com>
2019-09-11 00:26:59 +0800

    Update UserQzoneRelationChain.js

--- UserQzoneRelationChain.js
+++ UserQzoneRelationChain.js
@@ -1,4 +1,7 @@
 document.domain="qq.com";
-setTimeout('var a = window.open("https://user.qzone.qq.com/troubleshooter?traytip")',4000);
-setTimeout('alert(a.localStorage.getItem("app_friends_649114132"))',5000);
+function openProxy(){
+  var a = window.open("https://user.qzone.qq.com/troubleshooter?traytip");
+  a.onload = a.localStorage.getItem("app_friends_649114132")
+}
+setTimeout('openProxy()',4000);
 
