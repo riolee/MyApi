940fb6174956ae41e1251d51586bfbc8d6d4ee68
Camaro <cstcamaro@gmail.com>
2019-09-16 17:26:30 +0800

    Create babyPath.py

---
+++ babyPath.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+import time
+import requests
+
+
+def checkFlag():
+    setFlagFalse()
+    url = 'http://127.0.0.1:81/backup/'
+    s = requests.get(url)
+    status = s.status_code
+    if (status == 403):
+        setFlagTrue()
+
+def setFlagTrue():
+    # print("set flag true")
+    with open(filename) as f:
+        file_str = f.read()
+    with open(filename, "w") as f:
+        f.write(file_str.replace("该路径未关闭", "该路径已关闭"))
+
+def setFlagFalse():
+    with open(filename) as f:
+        file_str = f.read()
+    with open(filename, "w") as f:
+        f.write(file_str.replace("该路径已关闭", "该路径未关闭"))
+
+while 1:
+    filename = "index.html"
+    try:
+        checkFlag()
+    except:
+        None
+    time.sleep(1)
