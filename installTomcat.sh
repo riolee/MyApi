a48b06dab1b7034682a9aed3313e12535cbea3a7
Camaro <cstcamaro@gmail.com>
2019-09-18 15:59:37 +0800

    Update installTomcat.sh

--- installTomcat.sh
+++ installTomcat.sh
@@ -1,15 +1,14 @@
 sudo apt-get update
 sudo apt-get install default-jdk
 sudo groupadd tomcat
+sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
 cd /tmp
-curl -O http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.zip
+curl -O http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.tar.gz
 sudo mkdir /opt/tomcat
-sudo tar xzvf apache-tomcat-8.5.45.zip -C /opt/tomcat --strip-components=1
+sudo tar apache-tomcat-8.5.45.tar.gz -C /opt/tomcat --strip-components=1
 cd /opt/tomcat
 sudo chgrp -R tomcat /opt/tomcat
 sudo chmod -R g+r conf
 sudo chmod g+x conf
+sudo chown -R tomcat webapps/ work/ temp/ logs/
 sudo update-java-alternatives -l
-sudo systemctl daemon-reload
-sudo systemctl enable tomcat
-sudo systemctl start tomcat
