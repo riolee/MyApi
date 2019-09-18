6188d1b93bf8f80eae8fee607daa78548d4e8cb5
Camaro <cstcamaro@gmail.com>
2019-09-18 15:02:19 +0800

    Update installTomcat.sh

--- installTomcat.sh
+++ installTomcat.sh
@@ -4,7 +4,7 @@ sudo groupadd tomcat
 cd /tmp
 curl -O http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.zip
 sudo mkdir /opt/tomcat
-sudo tar xzvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1
+sudo tar xzvf apache-tomcat-8.5.45.zip -C /opt/tomcat --strip-components=1
 cd /opt/tomcat
 sudo chgrp -R tomcat /opt/tomcat
 sudo chmod -R g+r conf
